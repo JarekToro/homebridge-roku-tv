d80b3f6f7219f7d610833c95f19b05bbe27ee243
Jarek Toro <jarek.toro.b@gmail.com>
2020-05-17 08:02:01 -0400

    init

---
+++ src/settings.ts
@@ -0,0 +1,9 @@
+/**
+ * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
+ */
+export const PLATFORM_NAME = 'RokuTV';
+
+/**
+ * This must match the name of your plugin as defined the package.json
+ */
+export const PLUGIN_NAME = 'roku-tv-plugin';
